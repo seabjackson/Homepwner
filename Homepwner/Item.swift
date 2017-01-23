490c58e271a5ef71b96147e33eb6879c9bc55cd6
Seab <honeyboyseab@gmail.com>
2017-01-22 23:45:31 -0500

    added convenience and designated initializers for the Item Class

---
+++ Homepwner/Item.swift
@@ -0,0 +1,99 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Seab on 1/22/17.
+//  Copyright © 2017 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        
+        // once all the items in the class are given a value, the designated initializer calls its parent or super class
+        super.init()
+    }
+    
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(idx)]
+            
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName,
+                      serialNumber: randomSerialNumber,
+                      valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Seab on 1/22/17.
+//  Copyright © 2017 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
