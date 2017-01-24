5ba8f802d537000ed08144bfe0e0916c6fb516c7
Seab <honeyboyseab@gmail.com>
2017-01-23 22:02:11 -0500

    fixed the issue where the content was underlapping the status bar

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -15,7 +15,12 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // tet the height of the status bar
+        // get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
         
     }
     
