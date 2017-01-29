f16c1bc9eb02f43f96a496a3843484a65d54a21b
Seab <honeyboyseab@gmail.com>
2017-01-28 22:44:25 -0500

    updated error handling in the event that a photo cannot be deleted or saved

--- Homepwner/Homepwner/ImageStore.swift
+++ Homepwner/Homepwner/ImageStore.swift
@@ -12,15 +12,50 @@ class ImageStore {
     let cache = NSCache<NSString, UIImage>()
     
     func setImage(_ image: UIImage, forKey key: String) {
-       cache.setObject(image, forKey: key as NSString)
+        cache.setObject(image, forKey: key as NSString)
+        
+        // create full URL for image
+         let url = imageURL(forKey: key)
+        
+        // turn image into jpeg data
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            // write it to full URL
+            let _ = try? data.write(to: url, options: [.atomic])
+        }
+        
+        
     }
     
     func image(forKey key: String) -> UIImage? {
-        return cache.object(forKey: key as NSString)
+        if let existingImage = cache.object(forKey: key as NSString) {
+            return existingImage
+        }
+        
+        let url = imageURL(forKey: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key as NSString)
+        return imageFromDisk
     }
     
     func deleteImage(forKey key: String) {
         cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(forKey: key)
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
+    }
+    
+    // we should save the images in the documents directory
+    func imageURL(forKey key: String) -> URL {
+        let documentDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentDirectories.first!
+        return documentDirectory.appendingPathComponent(key)
     }
 }
 
